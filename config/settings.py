5b14fba95f55c255ee8fd0b51870e11c68680a0e
kora <radmehrkooshan@gmail.com>
2024-11-16 00:51:08 +0330

    infstr done

--- config/settings.py
+++ config/settings.py
@@ -33,10 +33,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Celery
-
-CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 # Application definition
 
